dad3da85360720555dcdb7d34069df786806eea4
ilnur14evro-code <ilnur14evro@gmail.com>
2026-08-20 01:11:42 +0300

    fix: initialize enemy on distinct walkable tile

--- src/engine/world/world.c
+++ src/engine/world/world.c
@@ -4,6 +4,31 @@ static int world_in_bounds(const World *world, int x, int y) {
     return x >= 0 && y >= 0 && x < world->width && y < world->height;
 }
 
+static int world_find_free_tile(const World *world, Position origin, Position *result) {
+    int radius;
+
+    if (world == 0 || result == 0) {
+        return 0;
+    }
+
+    for (radius = 1; radius < WORLD_MAX_WIDTH + WORLD_MAX_HEIGHT; ++radius) {
+        int offsets[4][2] = {{radius, 0}, {-radius, 0}, {0, radius}, {0, -radius}};
+        int i;
+
+        for (i = 0; i < 4; ++i) {
+            int x = origin.x + offsets[i][0];
+            int y = origin.y + offsets[i][1];
+            if (world_is_walkable(world, x, y)) {
+                result->x = x;
+                result->y = y;
+                return 1;
+            }
+        }
+    }
+
+    return 0;
+}
+
 void world_init(World *world, int width, int height) {
     int x;
     int y;
@@ -21,6 +46,8 @@ void world_init(World *world, int width, int height) {
 
     world->width = width;
     world->height = height;
+    world->player = ENTITY_INVALID;
+    world->enemy = ENTITY_INVALID;
     entity_store_init(&world->entities);
 
     for (y = 0; y < height; ++y) {
@@ -34,17 +61,9 @@ void world_init(World *world, int width, int height) {
     player_position.y = height / 2;
     world->player = entity_store_create(&world->entities, player_position);
 
-    enemy_position.x = player_position.x + 2;
-    enemy_position.y = player_position.y;
-    if (!world_is_walkable(world, enemy_position.x, enemy_position.y)) {
-        enemy_position.x = player_position.x - 2;
+    if (world_find_free_tile(world, player_position, &enemy_position)) {
+        world->enemy = entity_store_create(&world->entities, enemy_position);
     }
-    if (!world_is_walkable(world, enemy_position.x, enemy_position.y)) {
-        enemy_position.x = player_position.x;
-        enemy_position.y = player_position.y + 1;
-    }
-
-    world->enemy = entity_store_create(&world->entities, enemy_position);
 }
 
 int world_is_walkable(const World *world, int x, int y) {
