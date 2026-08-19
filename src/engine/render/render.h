d7e6f449c39c9d45301a46187c2533d5fbf5dc89
ilnur14evro-code <ilnur14evro@gmail.com>
2026-08-20 01:03:45 +0300

    fix: isolate engine renderer with RenderBuffer

--- src/engine/render/render.h
+++ src/engine/render/render.h
@@ -2,8 +2,17 @@
 #define GAME_ENGINE_RENDER_RENDER_H
 
 #include "engine/world/world.h"
-#include "platform/terminal/terminal.h"
 
-void render_game(Terminal *terminal, const World *world);
+#define RENDER_MAX_WIDTH 128
+#define RENDER_MAX_HEIGHT 128
+
+typedef struct {
+    int width;
+    int height;
+    char cells[RENDER_MAX_HEIGHT][RENDER_MAX_WIDTH];
+} RenderBuffer;
+
+void render_buffer_clear(RenderBuffer *buffer, int width, int height, char fill);
+void render_world(const World *world, RenderBuffer *buffer);
 
 #endif
