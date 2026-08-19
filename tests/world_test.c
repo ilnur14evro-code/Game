0ffd47a428fe4dbbd280469b9401b5a8b4238cce
ilnur14evro-code <ilnur14evro@gmail.com>
2026-08-20 01:10:35 +0300

    test: update world tests for entities

--- tests/world_test.c
+++ tests/world_test.c
@@ -4,21 +4,21 @@
 
 int main(void) {
     World world;
+    Position position;
 
     world_init(&world, 10, 6);
     assert(world.width == 10);
     assert(world.height == 6);
-    assert(world.player.x == 5);
-    assert(world.player.y == 3);
+    assert(entity_store_get_position(&world.entities, world.player, &position) == 1);
+    assert(position.x == 5);
+    assert(position.y == 3);
+    assert(entity_store_get_position(&world.entities, world.enemy, &position) == 1);
+    assert(position.x == 7);
+    assert(position.y == 3);
 
-    assert(world_try_move_player(&world, 1, 0) == 1);
-    assert(world.player.x == 6);
-    assert(world_try_move_player(&world, 0, -100) == 0);
-
-    world.player.x = 1;
-    world.player.y = 1;
-    assert(world_try_move_player(&world, -1, 0) == 0);
-    assert(world.player.x == 1);
+    assert(world_is_walkable(&world, 5, 3) == 1);
+    assert(world_is_walkable(&world, 0, 0) == 0);
+    assert(world_is_walkable(&world, -1, 0) == 0);
 
     return 0;
 }
