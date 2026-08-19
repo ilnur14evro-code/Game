354c0bb40791da3a0c5a6d48bdfcc48f654c8aa4
ilnur14evro-code <ilnur14evro@gmail.com>
2026-08-20 01:02:04 +0300

    feat: add renderer API

---
+++ src/engine/render/render.h
@@ -0,0 +1,9 @@
+#ifndef GAME_ENGINE_RENDER_RENDER_H
+#define GAME_ENGINE_RENDER_RENDER_H
+
+#include "engine/world/world.h"
+#include "platform/terminal/terminal.h"
+
+void render_game(Terminal *terminal, const World *world);
+
+#endif
