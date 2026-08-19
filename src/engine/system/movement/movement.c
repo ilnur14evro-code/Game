15eb0c608721ee9087a05baa3416a995e4e71993
ilnur14evro-code <ilnur14evro@gmail.com>
2026-08-20 01:35:39 +0300

    feat: publish movement events

--- src/engine/system/movement/movement.c
+++ src/engine/system/movement/movement.c
@@ -1,9 +1,10 @@
 #include "engine/system/movement/movement.h"
 
-int movement_system_move(World *world, EntityStore *store, EntityId entity, int dx, int dy) {
+int movement_system_move(World *world, EntityStore *store, EntityId entity, int dx, int dy, EventQueue *events) {
     Position position;
     Position next;
     EntityId occupant;
+    Event moved;
 
     if (world == 0 || store == 0 || !entity_store_get_position(store, entity, &position)) {
         return 0;
@@ -20,5 +21,19 @@ int movement_system_move(World *world, EntityStore *store, EntityId entity, int 
         return 0;
     }
 
-    return entity_store_set_position(store, entity, next);
+    if (!entity_store_set_position(store, entity, next)) {
+        return 0;
+    }
+
+    if (events != 0) {
+        moved.type = EVENT_ENTITY_MOVED;
+        moved.entity = entity;
+        moved.target = ENTITY_INVALID;
+        moved.x = next.x;
+        moved.y = next.y;
+        moved.value = 0;
+        (void)event_queue_push(events, moved);
+    }
+
+    return 1;
 }
