72733a732b2c40bda33b199ece83c536294aebc5
ilnur14evro-code <ilnur14evro@gmail.com>
2026-08-20 01:06:31 +0300

    feat: add simple entity position storage

---
+++ src/engine/entity/entity_store.h
@@ -0,0 +1,18 @@
+#ifndef GAME_ENGINE_ENTITY_ENTITY_STORE_H
+#define GAME_ENGINE_ENTITY_ENTITY_STORE_H
+
+#include "engine/entity/entity.h"
+#include "engine/world/world.h"
+
+typedef struct {
+    int alive[ENTITY_MAX];
+    Position positions[ENTITY_MAX];
+} EntityStore;
+
+void entity_store_init(EntityStore *store);
+EntityId entity_store_create(EntityStore *store, Position position);
+int entity_store_is_alive(const EntityStore *store, EntityId id);
+int entity_store_get_position(const EntityStore *store, EntityId id, Position *position);
+int entity_store_set_position(EntityStore *store, EntityId id, Position position);
+
+#endif
