54578480528ea21b9780c69204b9fd0f26b158eb
ilnur14evro-code <ilnur14evro@gmail.com>
2026-08-20 01:11:20 +0300

    test: verify entity rendering

--- tests/render_test.c
+++ tests/render_test.c
@@ -5,19 +5,20 @@
 int main(void) {
     World world;
     RenderBuffer buffer;
-    int cx;
-    int cy;
+    Position player;
+    Position enemy;
 
     world_init(&world, 8, 6);
-    cx = world.player.x;
-    cy = world.player.y;
+    assert(entity_store_get_position(&world.entities, world.player, &player) == 1);
+    assert(entity_store_get_position(&world.entities, world.enemy, &enemy) == 1);
 
     render_world(&world, &buffer);
 
     assert(buffer.width == 8);
     assert(buffer.height == 6);
     assert(buffer.cells[0][0] == '#');
-    assert(buffer.cells[cy][cx] == '@');
+    assert(buffer.cells[player.y][player.x] == '@');
+    assert(buffer.cells[enemy.y][enemy.x] == 'g');
     assert(buffer.cells[1][1] == '.');
 
     return 0;
