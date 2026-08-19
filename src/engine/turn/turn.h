7b06c910e1ff7444f94c55f96734b2c3ce563251
ilnur14evro-code <ilnur14evro@gmail.com>
2026-08-20 01:24:49 +0300

    refactor: introduce explicit turn manager

--- src/engine/turn/turn.h
+++ src/engine/turn/turn.h
@@ -4,6 +4,19 @@
 #include "engine/command/command.h"
 #include "engine/world/world.h"
 
+typedef enum {
+    TURN_PHASE_PLAYER = 0,
+    TURN_PHASE_ENEMY,
+    TURN_PHASE_COMPLETE
+} TurnPhase;
+
+typedef struct {
+    TurnPhase phase;
+    unsigned long turn_number;
+} TurnManager;
+
+void turn_manager_init(TurnManager *manager);
+int turn_manager_process(TurnManager *manager, World *world, Command command);
 int turn_process_command(World *world, Command command);
 
 #endif
