2dc46d5e0eb9c8cd639659438b7fcc948290bddd
ilnur14evro-code <ilnur14evro@gmail.com>
2026-08-20 01:21:36 +0300

    refactor: remove obsolete ai interface

--- src/engine/ai/ai.h
+++
@@ -1,9 +0,0 @@
-#ifndef GAME_ENGINE_AI_AI_H
-#define GAME_ENGINE_AI_AI_H
-
-#include "engine/entity/entity_store.h"
-#include "engine/world/world.h"
-
-int ai_take_turn(World *world, EntityStore *entities, EntityId entity, EntityId player);
-
-#endif
