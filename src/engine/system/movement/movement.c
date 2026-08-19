a76d63620b0d78ae5142566d7454c6ca88342b97
ilnur14evro-code <ilnur14evro@gmail.com>
2026-08-20 01:12:10 +0300

    fix: block movement into occupied entity tiles

--- src/engine/system/movement/movement.c
+++ src/engine/system/movement/movement.c
@@ -3,6 +3,7 @@
 int movement_system_move(World *world, EntityStore *store, EntityId entity, int dx, int dy) {
     Position position;
     Position next;
+    EntityId occupant;
 
     if (world == 0 || store == 0 || !entity_store_get_position(store, entity, &position)) {
         return 0;
@@ -12,6 +13,10 @@ int movement_system_move(World *world, EntityStore *store, EntityId entity, int 
     next.y = position.y + dy;
 
     if (!world_is_walkable(world, next.x, next.y)) {
+        return 0;
+    }
+
+    if (entity_store_find_at(store, next, &occupant) && occupant != entity) {
         return 0;
     }
 
