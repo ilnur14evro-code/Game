d45b6a9f74306259063400eb12925fdcffab8e39
ilnur14evro-code <ilnur14evro@gmail.com>
2026-08-20 01:03:48 +0300

    fix: remove platform dependency from engine core

--- src/engine/core/game.h
+++ src/engine/core/game.h
@@ -2,16 +2,17 @@
 #define GAME_ENGINE_CORE_GAME_H
 
 #include "engine/command/command.h"
+#include "engine/render/render.h"
 #include "engine/world/world.h"
-#include "platform/terminal/terminal.h"
 
 typedef struct {
     World world;
-    Terminal *terminal;
+    RenderBuffer render_buffer;
     int finished;
 } Game;
 
-int game_init(Game *game, Terminal *terminal);
+int game_init(Game *game);
 int game_process_key(Game *game, Key key);
+const RenderBuffer *game_render_buffer(const Game *game);
 
 #endif
