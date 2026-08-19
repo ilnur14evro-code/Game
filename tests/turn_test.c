43ca864b92d87fb1abf82e40de0ff7556607f4a9
ilnur14evro-code <ilnur14evro@gmail.com>
2026-08-20 01:25:30 +0300

    test: verify explicit turn manager phases

--- tests/turn_test.c
+++ tests/turn_test.c
@@ -4,17 +4,24 @@
 
 int main(void) {
     World world;
+    TurnManager manager;
     Command command;
     Position player;
     Position enemy_before;
     Position enemy_after;
 
     world_init(&world, 10, 6);
+    turn_manager_init(&manager);
+
+    assert(manager.phase == TURN_PHASE_PLAYER);
+    assert(manager.turn_number == 0);
     assert(entity_store_get_position(&world.entities, world.player, &player) == 1);
     assert(entity_store_get_position(&world.entities, world.enemy, &enemy_before) == 1);
 
     command.type = COMMAND_MOVE_RIGHT;
-    assert(turn_process_command(&world, command) == 1);
+    assert(turn_manager_process(&manager, &world, command) == 1);
+    assert(manager.phase == TURN_PHASE_PLAYER);
+    assert(manager.turn_number == 1);
     assert(entity_store_get_position(&world.entities, world.player, &player) == 1);
     assert(player.x == 6);
     assert(entity_store_get_position(&world.entities, world.enemy, &enemy_after) == 1);
@@ -22,9 +29,11 @@ int main(void) {
     assert(enemy_after.y == enemy_before.y);
 
     command.type = COMMAND_WAIT;
-    assert(turn_process_command(&world, command) == 1);
+    assert(turn_manager_process(&manager, &world, command) == 1);
+    assert(manager.turn_number == 2);
 
-    assert(turn_process_command(&world, (Command){COMMAND_NONE}) == 0);
+    assert(turn_manager_process(&manager, &world, (Command){COMMAND_NONE}) == 0);
+    assert(manager.turn_number == 2);
 
     return 0;
 }
