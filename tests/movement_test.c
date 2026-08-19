f6f797d760e206f40859b2e199d31e9ad12e4166
ilnur14evro-code <ilnur14evro@gmail.com>
2026-08-20 01:30:08 +0300

    test: restore movement system coverage

---
+++ tests/movement_test.c
@@ -0,0 +1,26 @@
+#include "engine/system/movement/movement.h"
+
+#include <assert.h>
+
+int main(void) {
+    World world;
+    Position position;
+
+    world_init(&world, 10, 6);
+
+    assert(entity_store_get_position(&world.entities, world.player, &position) == 1);
+    assert(position.x == 5);
+    assert(position.y == 3);
+
+    assert(movement_system_move(&world, &world.entities, world.player, 1, 0) == 1);
+    assert(entity_store_get_position(&world.entities, world.player, &position) == 1);
+    assert(position.x == 6);
+    assert(position.y == 3);
+
+    assert(movement_system_move(&world, &world.entities, world.player, -100, 0) == 0);
+    assert(entity_store_get_position(&world.entities, world.player, &position) == 1);
+    assert(position.x == 6);
+
+    assert(movement_system_move(&world, &world.entities, world.player, -2, 0) == 0);
+    return 0;
+}
