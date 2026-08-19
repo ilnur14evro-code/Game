202c64135ab4730803729804fd5c8c8b76fc0d47
ilnur14evro-code <ilnur14evro@gmail.com>
2026-08-20 01:06:22 +0300

    feat: add entity id primitive

---
+++ src/engine/entity/entity.h
@@ -0,0 +1,11 @@
+#ifndef GAME_ENGINE_ENTITY_ENTITY_H
+#define GAME_ENGINE_ENTITY_ENTITY_H
+
+#include <stdint.h>
+
+typedef uint32_t EntityId;
+
+#define ENTITY_INVALID ((EntityId)0)
+#define ENTITY_MAX 32
+
+#endif
