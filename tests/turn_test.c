61f8dd5973c0fd7cced945a81f6933646847af4d
ilnur14evro-code <ilnur14evro@gmail.com>
2026-08-20 01:10:41 +0300

    test: verify player and enemy turn order

--- tests/turn_test.c
+++ tests/turn_test.c
@@ -5,26 +5,25 @@
 int main(void) {
     World world;
     Command command;
+    Position player;
+    Position enemy_before;
+    Position enemy_after;
 
     world_init(&world, 10, 6);
+    assert(entity_store_get_position(&world.entities, world.player, &player) == 1);
+    assert(entity_store_get_position(&world.entities, world.enemy, &enemy_before) == 1);
 
     command.type = COMMAND_MOVE_RIGHT;
     assert(turn_process_command(&world, command) == 1);
-    assert(world.player.x == 6);
-
-    command.type = COMMAND_MOVE_UP;
-    assert(turn_process_command(&world, command) == 1);
-    assert(world.player.y == 2);
+    assert(entity_store_get_position(&world.entities, world.player, &player) == 1);
+    assert(player.x == 6);
+    assert(entity_store_get_position(&world.entities, world.enemy, &enemy_after) == 1);
+    assert(enemy_after.x == enemy_before.x - 1);
 
     command.type = COMMAND_WAIT;
     assert(turn_process_command(&world, command) == 1);
 
-    world.player.x = 1;
-    world.player.y = 1;
-    command.type = COMMAND_MOVE_LEFT;
-    assert(turn_process_command(&world, command) == 0);
-    assert(world.player.x == 1);
-    assert(world.player.y == 1);
+    assert(turn_process_command(&world, (Command){COMMAND_NONE}) == 0);
 
     return 0;
 }
